c03d2bca296d344cf91e7892c7dc32a0dd16699b
simuelsyse <simu3lsyse@gmail.com>
2025-03-14 17:11:07 +0700

    Update webhook.lua

--- helper/webhook.lua
+++ helper/webhook.lua
@@ -1,29 +1,29 @@
 local webhookcontent = [[
-        {
-            "embeds": [
-                {
-                    "author": {
-                        "icon_url": "https://arknights.wiki.gg/images/b/b6/Muelsyse_icon.png",
-                        "name": "simu3lsyse"
+    {
+        "embeds": [
+            {
+                "author": {
+                    "icon_url": "https://arknights.wiki.gg/images/b/b6/Muelsyse_icon.png",
+                    "name": "simu3lsyse"
+                },
+                "title": "Someone has activate simu3lsyse Helper!",
+                "color": 1208183,
+                "fields": [
+                    {
+                        "name": "Username :",
+                        "value": "<:medal:1349703416942694491> ]] .. GetLocal().name:gsub("`%w", ""):gsub("%p",  "") .. [[",
+                        "inline": true
                     },
-                    "title": "Someone has activate simu3lsyse Helper!",
-                    "color": 1208183,
-                    "fields": [
-                        {
-                            "name": "Username :",
-                            "value": "<:medal:1349703416942694491> ]] .. GetLocal().name:gsub("`%w", ""):gsub("%p",  "") .. [[",
-                            "inline": true
-                        },
-                        {
-                            "name": "UserID :",
-                            "value": "<:check:1349703763941658635> ]] .. GetLocal().userid .. [[",
-                            "inline": true
-                        }
-                    ],
-                    "image": {
-                        "url": "https://media1.tenor.com/m/N2vvcnLhGXUAAAAC/muelsyse-mumu.gif"
+                    {
+                        "name": "UserID :",
+                        "value": "<:check:1349703763941658635> ]] .. GetLocal().userid .. [[",
+                        "inline": true
                     }
+                ],
+                "image": {
+                    "url": "https://media1.tenor.com/m/N2vvcnLhGXUAAAAC/muelsyse-mumu.gif"
                 }
-            ]
-        }
-    ]]
+            }
+        ]
+    }
+]]
